96bc8e81131c0bac0454ddd9c7ddc424a286f0a9
fwrnandaaa <claudiazkn432@gmail.com>
2023-08-07 11:26:53 -0300

    tentando ajeitar navbar

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -8,9 +8,13 @@ export default function Cabecalho(props) {
 {/* esse bclassname vai servir para estilizar o
  titulo na file cabecalho.mpodule.css */}
 <div className = {styles.cabecalho}>
-    <p className={styles.titulo} >{props.titulo}</p>
+<div className = {styles.teste}>
+<p className={styles.titulo} >{props.titulo}</p>
+<img className = {`${styles.titulo}, ${styles.img}`}src= {props.logo}></img> 
+    
+    </div>
     <p className={styles.subtitulo}>{props.subtitulo}</p>
-    <img className = {`${styles.titulo}, ${styles.img}`}src= {props.logo}></img> 
+  
     </div>
     </>
     //props é o objeto q recebe as propriedades escrita no titulo e no subtitulo na file cabecalho
