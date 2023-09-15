446a5e7f8d4e7d29ef2c1ab2445e2b1035dd54c8
fwrnandaaa <claudiazkn432@gmail.com>
2023-09-15 09:35:40 -0300

    formulario

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 export default function Conteudo() {
+
   return (
-    <div></div>
+    
+    <div>
+        <form> onSubmit={()=>alert("Favorito salvo")}
+    <label> 
+      Nome do site
+    </label>
+    <input name='nome_site'> </input>
+    <label>
+      URL
+    </label>
+   </form>
+    </div>
   )
 }
 /* linha 3 esta criando uma função com o nome conteudo.
  export = exponhe a funçãopara o resto do codigo; default= função padrão, so é possivel ter uma,
   que nesse caso é a função Conteudo */
-  export function OlaMundo(){
-    console.log("Ola mundo")
-  }
+ 
