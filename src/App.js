d2ff078bd46d3d262c1b590b133025c6c75b6823
fwrnandaaa <claudiazkn432@gmail.com>
2023-10-20 08:23:09 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import "./App.css";
-import   Cadastro  from "./componentes/Cadastrousuario";
+import Cadastro from "./componentes/Cadastrousuario";
 import Login from "./componentes/LoginUsuario";
 import Cabecalho from "./componentes/Cabecalho";
 import Conteudo from "./componentes/Conteudo2";
-//import Conteudo, { OlaMundo } from "./componentes/Conteudo";
 import Rodape from "./componentes/Rodape";
 import Estados from "./exemplos/Estados";
 
 
 
 function App() {
- 
- // OlaMundo();
+
   return (
-    
+
     <>
 
       {/* {<Estados />} */}
-      <Cadastro/> 
-    <Login/>
+      <Cadastro />
+      <Login />
       {/* <Cabecalho
         logo="logo192.png"
         titulo="REACT"
--- src/componentes/Cadastrousuario/index.js
+++ src/componentes/Cadastrousuario/index.js
@@ -3,68 +3,64 @@ import styles from './Cadastrousuario.module.css'
 
 const Cadastro = () => {
 
-    const [senha, setSenha]=useState('')
-    const [nome, setNome]=useState('')
-    const [email, setEmail]=useState('')
-    const [confSenha, setConfSenha]=useState('')
-    const [salvar, setSalvar]=useState([])
-  
+    const [senha, setSenha] = useState('')
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [confSenha, setConfSenha] = useState('')
+    const [salvar, setSalvar] = useState([])
+
     useEffect(() => {
-      localStorage.setItem('salvar', JSON.stringify(salvar));
+        localStorage.setItem('salvar', JSON.stringify(salvar));
     }, [salvar])
 
 
-    function salvarUsuario(){
+    function salvarUsuario() {
         if (senha === confSenha) {
-        const usuarioExiste = salvar.find((usuario) => usuario.email === email);
-    
-        if (usuarioExiste) {
-          alert("Usuário já está cadastrado. Faça login.");
-    
-        } else if (senha === confSenha) {
-          setSalvar([...salvar, {email, nome, senha}])
-          console.log('Dados Usuário>>', "Email", email, "Nome", nome)
-          localStorage.setItem("senha",JSON.stringify({salvar}))
-          console.log(salvar)
-          alert("Usuário cadastrado com sucesso!")
-    
-        } else {
-            
-          alert  ("As senha não coincidem");
+            const usuarioExiste = salvar.find((usuario) => usuario.email === email);
+
+            if (usuarioExiste) {
+                alert("Usuário já está cadastrado. Faça login.");
+
+            } else if (senha === confSenha) {
+                setSalvar([...salvar, { email, nome, senha }])
+                console.log('Dados Usuário>>', "Email", email, "Nome", nome)
+                localStorage.setItem("senha", JSON.stringify({ salvar }))
+                console.log(salvar)
+                alert("Usuário cadastrado com sucesso!")
+
+            } else {
+
+                alert("As senha não coincidem");
+            }
+
         }
-        
-    } 
-}return ( 
-    <>
-    <h1>Cadastro de Usuário</h1> 
+    } return (
+        <>
+            <h1>Cadastro de Usuário</h1>
 
-   <label> Email</label> 
-        <input name='email' onChange={(evento)=> 
-            {setEmail(evento.target.value);}}>
-        </input> <br/>
+            <label> Email</label>
+            <input name='email' onChange={(evento) => { setEmail(evento.target.value); }}>
+            </input> <br />
 
-    <label> Nome do usuário </label> 
-        <input name='nome' onChange={(evento)=> 
-            {setNome(evento.target.value);}}>
-        </input> <br/>
+            <label> Nome do usuário </label>
+            <input name='nome' onChange={(evento) => { setNome(evento.target.value); }}>
+            </input> <br />
 
-    <label> Senha </label> 
-        <input type='password' name='senha' onChange={(evento)=> 
-            {setSenha(evento.target.value);}}>
-        </input> <br/>
+            <label> Senha </label>
+            <input type='password' name='senha' onChange={(evento) => { setSenha(evento.target.value); }}>
+            </input> <br />
 
-    <label> Confirmação de senha  </label> 
-        <input type='password' name='confSenha' onChange={(evento)=> 
-            {setConfSenha(evento.target.value);}}>
-        </input> <br/>
+            <label> Confirmação de senha  </label>
+            <input type='password' name='confSenha' onChange={(evento) => { setConfSenha(evento.target.value); }}>
+            </input> <br />
 
-   <input type="button" value="Salvar" onClick={()=> salvarUsuario()}/>
+            <input type="button" value="Salvar" onClick={() => salvarUsuario()} />
 
 
 
 
-</>
-)    
+        </>
+    )
 
 
 
